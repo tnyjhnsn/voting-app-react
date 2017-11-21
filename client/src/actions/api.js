d3b12c643e09ccfe4dbee29663ec7b45faca4424
Tony Johnson <tony@tosp.net.au>
2017-11-21 15:54:12 +1100

    Express serves API and React

--- client/src/actions/api.js
+++ client/src/actions/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = () => {
   return axios.create({
-    baseURL: 'http://localhost:3005/'
+    // baseURL: 'http://localhost:3005/'
     // baseURL: 'http://www.tosp.net.au:3005/'
   })
 }
